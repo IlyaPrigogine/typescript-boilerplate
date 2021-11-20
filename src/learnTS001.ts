c92bbc51d2696403b8d45c91b5db5fee57a35f21
IlyaPrigogine <prigogine@protonmail.com>
2021-11-20 17:23:36 +0800

    submit

---
+++ src/learnTS001.ts
@@ -0,0 +1,18 @@
+export const flag = true;
+export const flagN = 1;
+export const flagStr  = 'hello';
+export enum flagColor {
+  Red,
+  Pink,
+  Blue,
+}
+export enum flagColorStr {
+  Red = 'red',
+  Pink = 'pink',
+  Blue = 'blue'
+}
+
+export const flagArray: number[] = [1,2,3];
+export const flagArray2 : Array<number> = [1,2,3];
+
+
--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -1,4 +1,5 @@
 import { greet } from '../main'
+import {flag, flagArray, flagArray2, flagColor, flagColorStr, flagN, flagStr} from "../learnTS001";
 
 test('the data is peanut butter', () => {
   expect(1).toBe(1)
@@ -11,3 +12,16 @@ test('greeting', () => {
 test('greeting', () => {
   expect(greet('Foo')).toBe('Hello Foo')
 });
+
+test ('test basic types', () => {
+  expect(flag).toBe(true);
+  expect(flagN).toBe(1);
+  expect(flagStr).toBe('hello');
+  expect(flagColor.Blue).toBe(2);
+  expect(flagColorStr.Pink).toBe('pink');
+});
+
+it ('test array types: ',() => {
+  expect(flagArray.length).toBe(3);
+  expect(flagArray2[1]).toBe(2);
+})
