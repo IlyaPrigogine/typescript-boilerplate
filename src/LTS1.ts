d77acd95c2d33d97814fca9154e90505dc4a6bc1
IlyaPrigogine <prigogine@protonmail.com>
2021-11-20 18:23:16 +0800

    added new funcs in LTS1.ts && test code for new funcs

--- src/LTS1.ts
+++ src/LTS1.ts
@@ -19,3 +19,45 @@ export const flagTuple : [string,number] = ['john',35];
 export const flagSym = Symbol('hello');
 
 export const hello = (name: string) => console.log('hello ' + name);
+
+export const name1 : string | number = 'john';
+export const name2 : string | number = 35;
+
+
+const someValue: any = 'this is a string';
+export const strLength : number = (<string>someValue).length;
+export const strLength2 : number = (someValue as string).length;
+
+type flag1 = { x: number};
+type flag2 = flag1 & {y: string};
+export const flag3: flag2 = {
+  x: 1,
+  y: 'hello',
+}
+
+export const double = (input: string | number | boolean) => {
+  if (typeof input === 'string') {
+    return input + input;
+  } else if (typeof input === 'number') {
+    return input * 2;
+  } else {
+    return !input;
+  }
+}
+
+export class Animal {
+  name!: string;
+}
+export class Bird extends  Animal {
+  fly !: number;
+}
+
+export const getName = (animal: Animal) => {
+  if (animal instanceof Bird) {
+    return animal.fly + 1;
+  } else {
+    return 'hello,' + animal.name;
+  }
+  return animal;
+}
+
--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -1,6 +1,8 @@
 import { greet } from '../main'
 import {
-  flag,
+  Animal, Bird,
+  double,
+  flag, flag3,
   flagArray,
   flagArray2,
   flagColor,
@@ -8,8 +10,8 @@ import {
   flagN,
   flagStr,
   flagSym,
-  flagTuple,
-  hello
+  flagTuple, getName,
+  hello, name1, name2, strLength
 } from "../LTS1";
 
 test('the data is peanut butter', () => {
@@ -50,3 +52,36 @@ it ('test hello() in LTS1',() => {
   hello('world');
   hello('john');
 })
+
+it ('test Union Types in LTS1', () => {
+  expect(name1).toBe('john');
+  expect(name2).toBe(35);
+})
+
+it ('test Types Assertion in LTS1', () => {
+  expect(strLength).toBeGreaterThanOrEqual(10);
+  expect(strLength).toBeGreaterThanOrEqual(10);
+  expect(flag3).not.toBeNull();
+  expect(flag3.x).toBe(1);
+  expect(flag3.y).toBe('hello');
+})
+
+it ('test double() in LTS1', () => {
+  expect(double(3)).toBe(6);
+  expect(double('abc')).toBe('abcabc');
+  expect(double(false)).toBe(true);
+});
+
+it ('test getName() in LTS1', () => {
+  const animal = new Animal();
+  animal.name = 'world';
+  expect(getName(animal)).toBe('hello,world');
+
+  const inBird = new Bird();
+  inBird.fly = 36;
+  expect(getName(inBird)).toBe(37);
+})
+
+
+
+
